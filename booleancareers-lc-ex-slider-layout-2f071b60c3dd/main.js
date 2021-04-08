a7a0c2629a5aad833086c81fe70e148bc88e05e1
FrancoCaneti <CAnetifranco95@hmail.com>
2021-04-08 17:26:01 +0200

    jquery

---
+++ booleancareers-lc-ex-slider-layout-2f071b60c3dd/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+//variabili
+
+var btnR = $('.next');
+var btnL = $('.prev');
+
+//navigazione con muose a destra
+
+btnR.click(function() {
+     console.log('destra');
+});
+//navigazione con muose a sinistra
+
+btnL.click(function(){
+   console.log('sinistra');
+});
+
+
+
+});
